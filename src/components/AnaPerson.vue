efacefa6d32381bbd84f45f8d60b6a397ab7d0e1
krhistopher <krhistopher@weon.com.br>
2023-01-11 16:25:38 -0300

    projeto não finalizado

---
+++ src/components/AnaPerson.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="AnaPerson">
+        <li>{{ oneValue }} {{ name }}</li>
+    </div>
+    
+</template>
+
+<script>
+
+export default {
+    data(){
+        return {
+            oneValue: 1,
+            name: 'Ana',
+            idade: 31
+        }
+    }
+    
+}
+</script>
+
+<style scoped>
+#AnaPerson li:hover {
+    background-color: rgba(204, 190, 190, 0.815);
+}
+
+</style>
---
+++ src/components/HomePage.vue
@@ -0,0 +1,20 @@
+<template>
+    <div id="homePage">
+        <AnaPerson/>
+    </div>
+    
+</template>
+
+<script>
+import AnaPerson from './AnaPerson.vue';
+export default {
+    components: {
+        AnaPerson
+    }
+    
+}
+</script>
+
+<style scoped>
+
+</style>
